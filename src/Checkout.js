5e5cb7e129868a25b17952a8ab7c4f93570437f2
Valeriu Saros <valeriu.saros@gmail.com>
2020-11-30 10:53:55 +0200

    Added Remove from Baslet and completed the checkout

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import "./Checkout.css";
+import { useStateValue } from './StateProvider';
 import Subtotal from "./Subtotal";
+import CheckoutProduct from "./CheckoutProduct";
 
 function Checkout() {
+    const [{ basket }, dispatch] = useStateValue();
+
     return (
         <div className="checkout">
             <div className="checkout__left">
-                <img 
+                <img
                     className="checkout__ad"
                     src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
                     alt="ad banner"
@@ -16,6 +20,16 @@ function Checkout() {
                     <h2 className="checkout__title">
                         Your shopping Basket
                     </h2>
+
+                    {basket.map(item => (
+                        <CheckoutProduct
+                            id={item.id}
+                            title={item.title}
+                            image={item.image}
+                            price={item.price}
+                            rating={item.rating}
+                        />
+                    ))}
                 </div>
             </div>
 
@@ -26,4 +40,4 @@ function Checkout() {
     )
 }
 
-export default Checkout
+export default Checkout;
