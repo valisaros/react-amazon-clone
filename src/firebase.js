263e0a014f294fbc6b84d659bd144d48de1424b9
Valeriu Saros <valeriu.saros@gmail.com>
2020-11-30 16:13:46 +0200

    Added user auth with firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,18 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyD9u58yJF4zx8CVXW9rUr9I8pBL4uNypUE",
-    authDomain: "clone-amaz-valisaros.firebaseapp.com",
-    databaseURL: "https://clone-amaz-valisaros.firebaseio.com",
-    projectId: "clone-amaz-valisaros",
-    storageBucket: "clone-amaz-valisaros.appspot.com",
-    messagingSenderId: "156772573725",
-    appId: "1:156772573725:web:3534e98c308e211fe65cf4"
-  };
+  apiKey: "AIzaSyD9u58yJF4zx8CVXW9rUr9I8pBL4uNypUE",
+  authDomain: "clone-amaz-valisaros.firebaseapp.com",
+  databaseURL: "https://clone-amaz-valisaros.firebaseio.com",
+  projectId: "clone-amaz-valisaros",
+  storageBucket: "clone-amaz-valisaros.appspot.com",
+  messagingSenderId: "156772573725",
+  appId: "1:156772573725:web:3534e98c308e211fe65cf4"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
